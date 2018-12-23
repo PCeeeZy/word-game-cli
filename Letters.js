db537c3a97b75567775fab640de05751e00d0574
AlaaAlhilly <alaa.naji.us@gmail.com>
2018-12-22 22:09:42 -0600

    added score files

--- Letters.js
+++ Letters.js
@@ -1,19 +1,12 @@
-
-function Letter( guessed, letter) {
-    // every letter starts out as false 
+function Letter( guessed, letter){
     this.guessed = false;
     this.letter = letter;
-    // returns whether the letter is truthy or falsey
     this.display=function(){
         if(this.guessed){
-            // returns the letter if letter is true 
             return this.letter;
         }
-        // returns a hyphen if letter is false 
         return '-';
     }
-    // check if guessed letter against the correct letters in the word 
-    // and changes the boolean value for this.guessed 
     this.check=function(guessedLetter){
         if(this.letter === guessedLetter && !this.guessed){
             this.guessed = true;

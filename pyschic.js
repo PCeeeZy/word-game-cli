dfbaa647b71b3ebcb065adc220ad5ba22281843e
AlaaAlhilly <alaa.naji.us@gmail.com>
2018-12-22 22:46:37 -0600

    finished psydo code

--- pyschic.js
+++ pyschic.js
@@ -1,10 +1,13 @@
 let letter = require('./Letters');
 function Character(letterToGuess){
     this.savedLetter = new letter(false,letterToGuess);
+   //function to call Letter display function
     this.displayLetter = function(){
     w = this.savedLetter.display()+' ';
     console.log(w);
     }
+    //function to call Letter check function against
+    //player guess
     this.checkLetter = function(guessedL){
         if(this.savedLetter.check(guessedL)){
             console.log("Correct!!!\n");
